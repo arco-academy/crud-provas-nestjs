b94b27c7f4e990f731f0a5947c143bf2f4eff908
arthurgmalheiros <arthurgoncalvesmalheiros@gmail.com>
2024-02-09 17:15:33 -0300

    feat: :sparkles: creating put and delete

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateQuestionDTO } from './dto/create-questions.dto';
 
 @Controller('questions')
@@ -26,5 +35,32 @@ export class QuestionsController {
     @Param('id') id: string,
   ): Promise<CreateQuestionDTO | null> {
     return this.questions.find((question) => question.id === id);
+  }
+
+  @Put(':id')
+  async updateFullQuestion(
+    @Param('id') id: string,
+    @Body() question: CreateQuestionDTO,
+  ): Promise<CreateQuestionDTO> {
+    const selectedQuestion = await this.getQuestionById(id);
+    if (!selectedQuestion) {
+      throw new NotFoundException();
+    }
+
+    this.questions = this.questions.map((q) => (q.id !== id ? q : question));
+    return question;
+  }
+
+  @Delete(':id')
+  async deleteFullQuestion(
+    @Param('id') id: string,
+  ): Promise<CreateQuestionDTO[]> {
+    const selectedQuestion = await this.getQuestionById(id);
+    if (!selectedQuestion) {
+      throw new NotFoundException();
+    }
+
+    this.questions = this.questions.filter((q) => q.id !== id);
+    return this.questions;
   }
 }
