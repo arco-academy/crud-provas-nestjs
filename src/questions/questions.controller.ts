92dd81cf5c41dc83d48b03e83bcd82101a244176
FelipeJustinoArcoTech <felipe.justino@arcotech.io>
2024-02-09 16:42:53 -0300

    feat: create getQuestions

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,12 +1,30 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateQuestionDTO } from './dto/create-questions.dto';
 
 @Controller('questions')
 export class QuestionsController {
+  private questions: CreateQuestionDTO[];
+  constructor() {
+    this.questions = [];
+  }
+
   @Post()
   async createQuestions(
     @Body() data: CreateQuestionDTO,
   ): Promise<CreateQuestionDTO> {
+    this.questions.push(data);
     return data;
+  }
+
+  @Get()
+  async getAllQuestions(): Promise<CreateQuestionDTO[]> {
+    return this.questions;
+  }
+
+  @Get(':id')
+  async getQuestionById(
+    @Param('id') id: string,
+  ): Promise<CreateQuestionDTO | null> {
+    return this.questions.find((question) => question.id === id);
   }
 }
