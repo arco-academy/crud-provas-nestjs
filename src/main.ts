d6569c4dc11ea52df2a75f3ed7266bd1b5fdeb25
Lucas Viana <contato@lucasviana.dev>
2024-04-05 11:45:13 -0300

    feat: adiciona dependência do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Provas')
+    .setDescription(
+      'API para CRUD de provas, usado no Chapter Day para estudar Nest',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger-ui', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
