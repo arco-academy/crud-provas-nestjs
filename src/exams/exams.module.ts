70fe6eff5862517350959f3ee02290e834ccf2ad
centeno230799 <gabriel@arcotech.io>
2024-07-05 11:05:32 -0300

    chore(*): adiciona QuestionService ao exams.module

--- src/exams/exams.module.ts
+++ src/exams/exams.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ExamsController } from './exams.controller';
 import { ExamService } from './exams.service';
 import { QuestionsModule } from '../questions/questions.module';
+import { QuestionService } from 'src/questions/questions.service';
 
 @Module({
   controllers: [ExamsController],
   imports: [QuestionsModule],
-  providers: [ExamService],
+  providers: [ExamService, QuestionService],
 })
 export class ExamsModule {}
